60a87e992f313e282492f6b3f8cd8d5e9aedb5dd
Steve Scott <steve@sitefinitysteve.com>
2015-12-14 11:17:45 -0500

    Initial Push for android, seems to load

--- index.android.js
+++ index.android.js
@@ -19,29 +19,42 @@ exports.init = function(appId, url, clientId, isAnonymous){
 
 exports.openHelpCenter = function (style){
 	if(isInitalized){
-		    var activity = frameModule.topmost().android.activity;
+	   var activity = frameModule.topmost().android.activity;
     
-    debugger;
     
-    var callback = new com.zendesk.service.ZendeskCallback({
-                        onSuccess: function(args){
-                            console.log("SUCCESS");
-                            debugger;
-                        },
-                        onError: function(error){
-                            console.log("FAILED");
-                            debugger;
-                        }
-                    });
-    debugger;
-    
-    com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.init(activity, 
-                                                            account.appId,  
-                                                            account.url, 
-                                                            account.clientId, 
-                                                            callback
-                                                         );
+        var MyZendeskCallback = com.zendesk.service.ZendeskCallback.extend({
+            onSuccess: function(args){
+                console.log("SUCCESS");
+                debugger;
+                if(isAnonymous){
+                    loadAnonUser();
+                }
+                
 
+                
+                // 1) Sets the configuration used by the Contact Zendesk component
+                //com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.setContactConfiguration(new SampleFeedbackConfiguration());
+        
+                // 2) Starts the Help Center component
+                new com.zendesk.sdk.support.SupportActivity.Builder().listCategories().show(activity);
+            },
+            onError: function(error){
+                console.log("FAILED");
+                debugger;
+            }
+        });
+        
+        var callback = new MyZendeskCallback();
+        
+        com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.init(activity, 
+                                                                account.url,  
+                                                                account.appId, 
+                                                                account.clientId, 
+                                                                callback
+                                                            );
+                                                            
+
+        
 	} else{
 		console.log("Zendesk account info not initalized, please call the init function on the module");	
 	}
@@ -51,6 +64,14 @@ exports.setLocale = function(locale) {
 
 }
 
+// #####################################################
+// ## METHODS
+// #####################################################
 function loadAnonUser(){
+    var anonymousIdentity = new com.zendesk.sdk.model.access.AnonymousIdentity.Builder().build();
+    com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.setIdentity(anonymousIdentity);
+}
 
+function getColor(color){
+	return new colorModule.Color(color).android;
 }
