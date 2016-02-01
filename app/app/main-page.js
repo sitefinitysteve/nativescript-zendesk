5ff743046c9f89d275ff6a5b049daa93a6f55504
Steve Scott <steve@sitefinitysteve.com>
2016-02-01 10:23:40 -0500

    Update App add plugin

--- app/app/main-page.js
+++ app/app/main-page.js
@@ -1,6 +1,19 @@
-var vmModule = require("./main-view-model");
+var viewModel = require("./main-view-model");
+var zendesk = require("nativescript-zendesk");
+
 function pageLoaded(args) {
-    var page = args.object;
-    page.bindingContext = vmModule.mainViewModel;
+    var appID = "";
+    var url = "";
+    var clientId = "";
+    var isAnon = true; //Only true supported atm
+    zendesk.init(appID, url, clientId, isAnon);
 }
-exports.pageLoaded = pageLoaded;
+
+
+exports.onLoadHelpCenter = function(args){
+    zendesk.openHelpCenter();
+}
+
+exports.onLoadContact = function(args){
+    zendesk.openContact();
+}
