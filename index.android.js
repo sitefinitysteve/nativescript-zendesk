5dcc4089e199d28234d116d88058cb6312fbdc46
Sebastian Chlan <schlan@zendesk.com>
2016-02-01 12:00:15 +0000

    Fix Android part
    - Make ContactZendeskActivity (zendesk.openContact) work
    - Update to Android SDK to version 1.5.0.3
    - Add method for enable logging
    - Refactor index.android.js

--- index.android.js
+++ index.android.js
@@ -3,11 +3,12 @@ var application = require("application");
 
 var isAnonymous = true;
 var isInitalized = false;
+var isLoggingEnabled = false;
 
 var account = {
 	appId: "",
-    url: "",
-    clientId: "",
+	url: "",
+	clientId: "",
 }
 
 exports.init = function(appId, url, clientId, isAnonymous){
@@ -18,109 +19,90 @@ exports.init = function(appId, url, clientId, isAnonymous){
 	isAnonymous = isAnonymous;
 }
 
+exports.logging = function(loggingEnabled){
+	isLoggingEnabled = loggingEnabled;
+}
+
 exports.openHelpCenter = function (style){
 	if(isInitalized){
-	   var activity = frameModule.topmost().android.activity;
-    
-    
-        var MyZendeskCallback = com.zendesk.service.ZendeskCallback.extend({
-            onSuccess: function(args){
-                if(isAnonymous){
-                    loadAnonUser();
-                }
+  	var activity = frameModule.topmost().android.activity;
 
-                var sampleConfig = getConfig();
+    var MyZendeskCallback = com.zendesk.service.ZendeskCallback.extend({
+    	onSuccess: function(args){
+	      	if(isAnonymous){
+	        	loadAnonUser();
+	        }
+	        new com.zendesk.sdk.support.SupportActivity.Builder().listCategories().show(activity);
+        },
+        onError: function(error){
+          console.log(error);
+        }
+      });
+      initSdk(activity, new MyZendeskCallback())
 
-                com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.setContactConfiguration(sampleConfig);
-                
-                new com.zendesk.sdk.support.SupportActivity.Builder().listCategories().show(activity);
-            },
-            onError: function(error){
-                console.log(error);
-            }
-        });
-        
-        var callback = new MyZendeskCallback();
-        
-        com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.init(activity, 
-                                                                account.url,  
-                                                                account.appId, 
-                                                                account.clientId, 
-                                                                callback
-                                                            );
-                                                            
-
-        
 	} else{
-		console.log("Zendesk account info not initalized, please call the init function on the module");	
+		console.log("Zendesk account info not initalized, please call the init function on the module");
 	}
 }
 
-//NOT WORKING
 exports.openContact = function(){
-    console.log("Not Implimented yet")
-    /*
-   if(isInitalized){
-	   var activity = frameModule.topmost().android.activity;
-    
-    
-        var MyZendeskCallback = com.zendesk.service.ZendeskCallback.extend({
-            onSuccess: function(args){
-                if(isAnonymous){
-                    loadAnonUser();
-                }
+  console.log("Not Implimented yet")
 
-                var sampleConfig = getConfig();
+	if(isInitalized){
+	 	var activity = frameModule.topmost().android.activity;
 
-                com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.setContactConfiguration(sampleConfig);
-                
-                var intent = new android.content.Intent(com.zendesk.sdk.feedback.ui.ContactZendeskActivity.class);
-                application.android.startActivity(intent); 
-            },
-            onError: function(error){
-                console.log(error);
-            }
-        });
-        
-        var callback = new MyZendeskCallback();
-        
-        com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.init(activity, 
-                                                                account.url,  
-                                                                account.appId, 
-                                                                account.clientId, 
-                                                                callback
-                                                            );
-                                                            
+	  var MyZendeskCallback = com.zendesk.service.ZendeskCallback.extend({
+      onSuccess: function(args){
+        if(isAnonymous){
+            loadAnonUser();
+        }
+				var intent = new android.content.Intent(activity, com.zendesk.sdk.feedback.ui.ContactZendeskActivity.class);
+				activity.startActivity(intent);
+      },
+      onError: function(error){
+        console.log(error);
+      }
+	  });
+		initSdk(activity, new MyZendeskCallback())
 
-        
 	} else{
-		console.log("Zendesk account info not initalized, please call the init function on the module");	
+		console.log("Zendesk account info not initalized, please call the init function on the module");
 	}
-    */
 }
 
 exports.setLocale = function(locale) {
-    com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.setDeviceLocale(locale); 
+	com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.setDeviceLocale(locale);
 }
 
 exports.setTheme = function(args){
-    console.log("nativescript-zendesk setTheme Not available on Android, use the readme to see how to theme via the manifest")
+  console.log("nativescript-zendesk setTheme Not available on Android, use the readme to see how to theme via the manifest")
 }
 
 // #####################################################
 // ## METHODS
 // #####################################################
+function initSdk(activity, callback){
+	com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.setContactConfiguration(getConfig());
+	com.zendesk.logger.Logger.setLoggable(isLoggingEnabled);
+	com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.init(activity,
+																													account.url,
+																													account.appId,
+																													account.clientId,
+																													callback
+																											);
+}
+
 function loadAnonUser(){
-    var anonymousIdentity = new com.zendesk.sdk.model.access.AnonymousIdentity.Builder().build();
-    com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.setIdentity(anonymousIdentity);
+  var anonymousIdentity = new com.zendesk.sdk.model.access.AnonymousIdentity.Builder().build();
+  com.zendesk.sdk.network.impl.ZendeskConfig.INSTANCE.setIdentity(anonymousIdentity);
 }
 
 function getConfig() {
-    var SampleFeedbackConfiguration = com.zendesk.sdk.feedback.impl.BaseZendeskFeedbackConfiguration.extend({
-        getRequestSubject: function() {
-            return "Feedback from our App";
-        }
-    });
-    
-    return new SampleFeedbackConfiguration();
+  var SampleFeedbackConfiguration = com.zendesk.sdk.feedback.impl.BaseZendeskFeedbackConfiguration.extend({
+      getRequestSubject: function() {
+          return "Feedback from our App";
+      }
+  });
+
+  return new SampleFeedbackConfiguration();
 }
